5092e71b35ee16061416df07295e3393a3ed3632
Mike Kruk <tamagokun@gmail.com>
2013-01-14 15:01:34 -0500

    whitespace

--- app/app.rb
+++ app/app.rb
@@ -7,18 +7,18 @@ module FrontEnd
 		set :public_folder, "#{dir}/public"
 		set :views, "#{dir}/views"
 		set :static, true
-		
+
 		def is_ajax_request?
-      if respond_to? :content_type
-        return true if request.xhr?
-      end
-      false
-    end
+			if respond_to? :content_type
+				return true if request.xhr?
+			end
+			false
+		end
 
 		def json_data(data)
-      content_type "application/json" if is_ajax_request?
-      data.to_json
-    end
+			content_type "application/json" if is_ajax_request?
+			data.to_json
+		end
 
 		get "/" do
 			slim :index
